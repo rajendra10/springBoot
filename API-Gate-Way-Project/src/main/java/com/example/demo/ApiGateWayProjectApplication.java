bc7d84f7f633c6f0f9e71ea514e2fa9eff8c02d3
rajendra10 <rajgoswamikec.com>
2025-06-30 17:24:22 +0530

    added GateWayAPI

---
+++ API-Gate-Way-Project/src/main/java/com/example/demo/ApiGateWayProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWayProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("GATE-WAY API");
+		SpringApplication.run(ApiGateWayProjectApplication.class, args);
+	}
+
+}
