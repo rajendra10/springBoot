dd0e2bd9cb4f21b19bca2e5c4602156be5dcab34
rajendra10 <rajgoswamikec.com>
2025-12-05 10:53:43 +0530

    lamda function

---
+++ ticket-booking-lambda/src/main/java/com/example/ticket/TicketBookingLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingLambdaApplication.class, args);
+	}
+
+}
---
+++ ticket-booking-lambda/src/main/java/com/example/ticket/controller/BookingController.java
@@ -0,0 +1,27 @@
+package com.example.ticket.controller;
+
+import com.example.ticket.model.Booking;
+import com.example.ticket.service.BookingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping({"/ticket-booking-lambda", "/default/ticket-booking-lambda"})
+public class BookingController {
+
+    private final BookingService service;
+
+    public BookingController(BookingService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Booking create(@RequestBody Booking booking) {
+        return service.create(booking);
+    }
+
+    @GetMapping
+    public List<Booking> list() {
+        return service.findAll();
+    }
+}
